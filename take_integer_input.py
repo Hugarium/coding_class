a9080fd76e8837dcaf26b19af0c9619b235684fe
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-08-27 14:00:12 +0000

    Add take_integer_input.py

---
+++ take_integer_input.py
@@ -0,0 +1,15 @@
+# Take an integer number from user
+integer_number = int(input("Enter a whone number: "))
+
+
+#Add two to the input taken from the user
+Phone_number = integer_number + 2
+
+# Display the output
+print(Phone_number)
+
+
+
+
+
+
