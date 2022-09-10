bd04b26db91f3a50ba67df34a010b3f258f6c54f
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-09-10 13:28:42 +0000

    add area_of_a_triangle.py

---
+++ area_of_a_triangle.py
@@ -0,0 +1,15 @@
+## find the area of a triangle
+
+##area of a triangle = 1/2bh
+
+##base - base of the triangle
+
+##height- height of the triagle
+
+base = float(input("enter the base of the triangle: "))
+
+height = float(input("enter the height of the triangle: "))
+
+area = 1/2 * base * height
+
+print(area)
