91978a5047752b5e8df580d99f9132e5fb92c149
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-09-10 13:29:40 +0000

    add area_of_a_circle.py

---
+++ area_of_circle.py
@@ -0,0 +1,19 @@
+#### area of a  circle
+
+## A=𝝅r**2
+
+## A- Area of a circle
+
+##𝝅- the value of pi
+
+##r-radius of a circle
+
+
+
+pi = 22/7
+
+radius = float(input("enter the radius of the circle: "))
+
+area = (pi*radius)**2
+
+print(area)
