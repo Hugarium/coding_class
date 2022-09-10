9726483101dd6962b6fd703ef0d4fd9d5f936cd5
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-09-10 13:30:20 +0000

    add density_of_a_substance.py

---
+++ density_of_a_substance.py
@@ -0,0 +1,19 @@
+## Question if find the density of a substance
+
+## Density- mass/volume
+
+## mass-mass of the substance
+
+## volume-volume of the substance
+
+mass = float(input("input the mass of the substance: "))
+
+volume = float(input("input the volume of the substance: "))
+
+density = mass * volume
+
+print(density)
+
+
+
+
