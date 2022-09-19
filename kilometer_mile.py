98cc2edb8f2216b7edc1475208424e4cb7248806
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-09-19 14:41:13 +0000

    add kilometer_mile.py

---
+++ kilometer_mile.py
@@ -0,0 +1,15 @@
+#convert from kilometer to miles
+
+#km = 0.6213711922m
+
+#km- kilometer
+
+#m - miles
+
+#km = 0.6213711922 * miles
+
+miles = float(input("enter the value of miles: "))
+
+kilometer = (0.6213711922 * miles)
+
+print(kilometer)
