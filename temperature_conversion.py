c8978daeb2db810bf9da4fafc05e2428751541fa
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-09-19 14:41:33 +0000

    add temperature_conversion.py

---
+++ temperature_conversion.py
@@ -0,0 +1,13 @@
+#convert from Degrees to fahrenheit
+
+#F = C (9/5) + 32
+
+#F - Fahrenheit
+
+#C - Degrees
+
+degrees = float(input("enter the value of degrees: "))
+
+fahrenheit = degrees * (9/5) + 32
+
+print(fahrenheit)
