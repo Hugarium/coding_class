3f56229b6a9d545d6d4164f62dd86cf27bb6a94e
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-09-19 14:41:54 +0000

    add try_conditions.py

---
+++ try_conditions.py
@@ -0,0 +1,24 @@
+#if , else,elif
+
+letter = input("enter a letter of the alphabet: ")
+
+if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+
+	print("letter is a vowel")
+
+elif letter == "y" or "h":
+
+	print("the letter is a vowel and sometimes a consonant")
+
+else:
+
+	print("the letter you entered is a consonant")
+
+
+
+
+
+
+
+
+
