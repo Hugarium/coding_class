2979f99c612aab0450dd77b63823a584aef4658f
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-12-03 12:39:47 +0000

    add functions_try.py

---
+++ functions_try.py
@@ -0,0 +1,35 @@
+#### Functions are names given to specific  set of instructions to execute a  specific task
+
+### Types of functions ###
+## Built in
+## user defined
+
+# Built in are functions
+# functions which are predefined in python
+# examples:
+#print()
+#input()
+#list()
+
+# user defined #
+## Functions created by the user
+## They are not predefined
+
+
+
+#### User defined fuctions syntax ##
+
+# def function_name(parameters):
+#function_body
+#return_output
+
+######### def function_name(parameter):      called function header
+######### function_body                      called instructions or #line of codes
+
+#return_output
+
+
+#def is a mandatory key to show its a user defined function
+#function_name should describe what the function does
+#() this is also  mandatory
+#parameters
