e386175b77546ae5328d0078aa05d61d16fee1c1
Hugarium <111638896+Hugarium@users.noreply.github.com>
2022-12-03 12:39:26 +0000

    add Dictionaries.trial.py

---
+++ Dictionaries.trial.py
@@ -0,0 +1,9 @@
+name_marks = {
+"Maxwell":{"maths":89, "agric":95}, 
+"Maggie":{"maths":89, "agric":99}
+"Francis":{"maths":86,"agric":56}
+"Luis":{"maths":93, "agric":98}
+"Prosper":{"maths":69, "agric":88}
+ }
+
+print(type(name_marks)
